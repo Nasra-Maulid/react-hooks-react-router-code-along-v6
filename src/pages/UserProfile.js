ca65708e6b5c13d99cedbcf45707d6fe681ae153
nasra <naasironasra@gmail.com>
2025-04-16 12:27:07 +0300

    added user profile

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+function UserProfile() {
+    return(
+        <>
+        <header>
+            <NavBar />
+            </header>
+            <mai>
+                <h1>User Profile</h1>
+            </mai>
+            </>
+    );
+};
+export default UserProfile;
